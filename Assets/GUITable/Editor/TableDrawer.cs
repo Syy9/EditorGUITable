4693a1dcba20096282bea4dbe07f1bd8cc8ee882
Jeremy Quentin <jeremy.quentin@gmail.com>
2018-01-14 22:19:09 +0800

    Add a PropertyAttribute to automatically display a collection in a table instead of usual array

---
+++ Assets/GUITable/Editor/TableDrawer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GUIExtensions;
+using System.Text.RegularExpressions;
+
+[CustomPropertyDrawer(typeof(TableAttribute))]
+public class TableDrawer : PropertyDrawer
+{
+
+	GUITableState tableState;
+
+	Rect lastRect;
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		//Check that it is a collection
+		Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
+		if (!match.Success)
+		{
+			EditorGUI.LabelField(position, label.text, "Use the Table attribute with a collection.");
+			return;
+		}
+
+		string collectionPath = match.Groups[1].Value;
+
+		// Check that it's the first element
+		string index = match.Groups[2].Value;
+
+		if (index != "0")
+			return;
+
+		if (GUILayoutUtility.GetLastRect().width > 1f)
+			lastRect = GUILayoutUtility.GetLastRect();
+		Rect r = new Rect(lastRect.x + 15f, lastRect.y + 35f, lastRect.width, lastRect.height);
+		GUILayout.BeginArea(r);
+		EditorGUI.indentLevel = 0;
+		tableState = GUITable.DrawTable(property.serializedObject.FindProperty(collectionPath), tableState);
+		GUILayout.EndArea();
+		GUILayout.Space(30f);
+	}
+}
