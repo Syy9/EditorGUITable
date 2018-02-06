beb67a47b36ba5b315a23b9e9d5ffde4eb78e790
MEDIA\Jeremy <jeremy@redrabbitanimation.cn>
2018-02-06 11:20:04 +0800

    Apply namespace to property drawer

--- Assets/GUITable/Editor/TableDrawer.cs
+++ Assets/GUITable/Editor/TableDrawer.cs
@@ -3,98 +3,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using EditorGUITable;
 using System.Text.RegularExpressions;
 
-/// <summary>
-/// Drawer for the Table Attribute.
-/// See the TableAttribute class documentation for the limitations of this attribute.
-/// </summary>
-[CustomPropertyDrawer(typeof(TableAttribute))]
-public class TableDrawer : PropertyDrawer
+namespace EditorGUITable
 {
 
-	GUITableState tableState;
-
-	Rect lastRect;
-
-	public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+	/// <summary>
+	/// Drawer for the Table Attribute.
+	/// See the TableAttribute class documentation for the limitations of this attribute.
+	/// </summary>
+	[CustomPropertyDrawer(typeof(TableAttribute))]
+	public class TableDrawer : PropertyDrawer
 	{
-		//Check that it is a collection
-		Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
-		if (!match.Success)
+
+		GUITableState tableState;
+
+		Rect lastRect;
+
+		public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
 		{
-			return EditorGUIUtility.singleLineHeight;
+			//Check that it is a collection
+			Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
+			if (!match.Success)
+			{
+				return EditorGUIUtility.singleLineHeight;
+			}
+
+			// Check that it's the first element
+			string index = match.Groups[2].Value;
+
+			if (index != "0")
+				return EditorGUIUtility.singleLineHeight;
+			
+			return 2 * EditorGUIUtility.singleLineHeight;
 		}
 
-		// Check that it's the first element
-		string index = match.Groups[2].Value;
-
-		if (index != "0")
-			return EditorGUIUtility.singleLineHeight;
-		
-		return 2 * EditorGUIUtility.singleLineHeight;
-	}
-
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-	{
-		TableAttribute tableAttribute = (TableAttribute) attribute;
-		if (tableAttribute.properties == null)
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			OnGUIAuto (position, property, label);
-		}
-		else
-		{
-			OnGUIPropList (position, property, label, tableAttribute.properties);
-		}
-	}
-
-	void OnGUIAuto(Rect position, SerializedProperty property, GUIContent label)
-	{
-		//Check that it is a collection
-		Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
-		if (!match.Success)
-		{
-			EditorGUI.LabelField(position, label.text, "Use the Table attribute with a collection.");
-			return;
+			TableAttribute tableAttribute = (TableAttribute) attribute;
+			if (tableAttribute.properties == null)
+			{
+				OnGUIAuto (position, property, label);
+			}
+			else
+			{
+				OnGUIPropList (position, property, label, tableAttribute.properties);
+			}
 		}
 
-		string collectionPath = match.Groups[1].Value;
-
-		// Check that it's the first element
-		string index = match.Groups[2].Value;
-
-		if (index != "0")
-			return;
-		EditorGUI.indentLevel = 0;
-		if (GUILayoutUtility.GetLastRect().width > 1f)
-			lastRect = GUILayoutUtility.GetLastRect();
-		Rect r = new Rect(position.x + 15f, position.y, position.width, lastRect.height);
-		tableState = GUITable.DrawTable(r, tableState, property.serializedObject.FindProperty(collectionPath), GUITableOption.AllowScrollView(false));
-	}
-
-	void OnGUIPropList(Rect position, SerializedProperty property, GUIContent label, string[] properties)
-	{
-		//Check that it is a collection
-		Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
-		if (!match.Success)
+		void OnGUIAuto(Rect position, SerializedProperty property, GUIContent label)
 		{
-			EditorGUI.LabelField(position, label.text, "Use the Table attribute with a collection.");
-			return;
+			//Check that it is a collection
+			Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
+			if (!match.Success)
+			{
+				EditorGUI.LabelField(position, label.text, "Use the Table attribute with a collection.");
+				return;
+			}
+
+			string collectionPath = match.Groups[1].Value;
+
+			// Check that it's the first element
+			string index = match.Groups[2].Value;
+
+			if (index != "0")
+				return;
+			EditorGUI.indentLevel = 0;
+			if (GUILayoutUtility.GetLastRect().width > 1f)
+				lastRect = GUILayoutUtility.GetLastRect();
+			Rect r = new Rect(position.x + 15f, position.y, position.width, lastRect.height);
+			tableState = GUITable.DrawTable(r, tableState, property.serializedObject.FindProperty(collectionPath), GUITableOption.AllowScrollView(false));
 		}
 
-		string collectionPath = match.Groups[1].Value;
+		void OnGUIPropList(Rect position, SerializedProperty property, GUIContent label, string[] properties)
+		{
+			//Check that it is a collection
+			Match match = Regex.Match(property.propertyPath, "^([a-zA-Z0-9_]*).Array.data\\[([0-9]*)\\]$");
+			if (!match.Success)
+			{
+				EditorGUI.LabelField(position, label.text, "Use the Table attribute with a collection.");
+				return;
+			}
 
-		// Check that it's the first element
-		string index = match.Groups[2].Value;
+			string collectionPath = match.Groups[1].Value;
 
-		if (index != "0")
-			return;
-		EditorGUI.indentLevel = 0;
-		if (GUILayoutUtility.GetLastRect().width > 1f)
-			lastRect = GUILayoutUtility.GetLastRect();
-		Rect r = new Rect(position.x + 15f, position.y, position.width, lastRect.height);
-		tableState = GUITable.DrawTable(r, tableState, property.serializedObject.FindProperty(collectionPath), properties.ToList(), GUITableOption.AllowScrollView(false));
+			// Check that it's the first element
+			string index = match.Groups[2].Value;
+
+			if (index != "0")
+				return;
+			EditorGUI.indentLevel = 0;
+			if (GUILayoutUtility.GetLastRect().width > 1f)
+				lastRect = GUILayoutUtility.GetLastRect();
+			Rect r = new Rect(position.x + 15f, position.y, position.width, lastRect.height);
+			tableState = GUITable.DrawTable(r, tableState, property.serializedObject.FindProperty(collectionPath), properties.ToList(), GUITableOption.AllowScrollView(false));
+		}
+
 	}
 
 }
