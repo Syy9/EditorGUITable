c8131519218ba897866f50327a0a6bfe094e42a7
Jeremy Quentin <jeremy.quentin@gmail.com>
2018-05-06 22:44:04 +0800

    Add Lite Example

---
+++ Assets/GUITable/Examples/Scripts/Editor/LiteExampleEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(LiteExample))]
+public class LiteExampleEditor : Editor 
+{
+
+	GUITableState tableState;
+
+	void OnEnable ()
+	{
+		tableState = new GUITableState("tableState_Lite");
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		GUILayout.Label ("Default display", EditorStyles.boldLabel);
+
+		base.OnInspectorGUI();
+
+		GUILayout.Space (20f);
+
+		GUILayout.Label ("Table display", EditorStyles.boldLabel);
+
+		tableState = GUITableLayout.DrawTable (tableState, serializedObject.FindProperty ("simpleObjects"));
+
+		GUILayout.Space (20f);
+
+	}
+
+}
