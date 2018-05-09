4ff9e7a4c302015ad4225935cb16926ccfaf2b98
Jeremy Quentin <jeremy.quentin@gmail.com>
2018-05-09 08:58:34 +0800

    Fix Test Library Project hierarchy

---
+++ MonoNetLibrary/TestLibrary/TestLibrary/Assets/GUITable/Examples/Scripts/LiteExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiteExample : MonoBehaviour 
+{
+
+	[System.Serializable]
+	public class SimpleObject
+	{
+		public string stringProperty;
+		public float floatProperty;
+		public GameObject objectProperty;
+	}
+
+	public List<SimpleObject> simpleObjects;
+
+	void OnGUI ()
+	{
+		GUILayout.Label ("Select the LiteExample scene object to visualize the table in the inspector");
+	}
+
+}
